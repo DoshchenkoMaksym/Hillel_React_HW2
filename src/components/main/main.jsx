ff66028deddc210528b5ed78407eb565adbd2cde
MaksymDoshchenko <doschenko54@gmail.com>
2022-02-06 18:47:31 +0200

    init

---
+++ src/components/main/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './main.module.css';
+
+class Main extends React.Component {
+
+    render() {
+
+        return (
+            <div className={style.main}>
+                <div>MAIN CONTENT</div>
+            </div>
+        )
+
+    };
+
+};
+
+export default Main;
